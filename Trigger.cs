57213e1f9fdefbd2a2fb03aa270a7bfc1a3e8394
Apathy <31334251+Dechathorn-in@users.noreply.github.com>
2022-02-10 01:05:26 +0700

    initial commit

---
+++ Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Scared;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Scared.SetActive(true);
+    }
+}
